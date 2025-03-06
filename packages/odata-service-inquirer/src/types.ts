9931e21a38a05fd11d8ac3e5e4e949ab58029a7b
Maruf Rasully <100434800+marufrasully@users.noreply.github.com>
2025-03-06 16:54:50 +0100

    test: 08

--- packages/odata-service-inquirer/src/types.ts
+++ packages/odata-service-inquirer/src/types.ts
@@ -292,6 +292,7 @@ export type SystemSelectionPromptOptions = {
      * Only show the default choice in the system selection prompt, this is used to skip the system selection prompt if the default choice is already known.
      * If the `defaultChoice` value is not found in the systems choices, or the `defaultChoice` option is not specified,
      * this option will not be applied and the full list of choices will be presented to the user.
+     * this should not trigger int tests.
      */
     onlyShowDefaultChoice?: boolean;
 };
